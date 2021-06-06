c3785bd190e4f5d4008a757e391a9c61ae99ae6f
Ghelonico Maligaya <ghelonicomaligaya111@gmail.com>
2021-06-06 20:15:09 +0800

    06/06/21 last commit

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,4 +1,5 @@
 var toggle = true;
+var toggleBG = true;
 
 function toggleSidebar() {
     if (toggle){
@@ -7,5 +8,17 @@ function toggleSidebar() {
     } else {
         document.getElementById("sideBar").style.width = "5%";
         this.toggle = true;
+    }
+}
+
+function changeBG() {
+    if (toggleBG){
+        document.body.style.backgroundColor = "magenta";
+        document.getElementById("name").style.color = "#105bfd";
+        this.toggleBG = false;
+    } else {
+        document.getElementById("name").style.color = "rgb(4, 33, 44)";
+        document.body.style.backgroundColor = "rgb(245, 239, 245)";
+        this.toggleBG = true;    
     }
 }
